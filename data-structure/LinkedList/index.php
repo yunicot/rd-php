b8313de654bf3ebfe49025d14a884b8ec0867987
Viacheslav Yepancha <viacheslav.yepancha@gmail.com>
2022-07-05 20:52:47 +0300

    execution time test

--- data-structure/LinkedList/index.php
+++ data-structure/LinkedList/index.php
@@ -4,18 +4,19 @@ require_once __DIR__ . '/Node.php';
 require_once __DIR__ . '/LinkedList.php';
 
 $list = new LinkedList();
-$list
-    ->insert(3)
-    ->insert(5)
-    ->insert(1)
-    ->insert(2)
-    ->insert(7)
-    ->insert(9)
-    ->insert(2)
-    ->insert(1)
-;
+$n = 100000;
+$list->insert(20000);
+for($i = 0; $i < $n; $i++) {
+    $list->insert(random_int(1, 10000));
+}
+
+$startAt = microtime(true);
+$list->search(20000);
+
+echo microtime(true) - $startAt . "\n";
+
 // 8->1->9->7->2->1->5->3
-$after = $list->search(7);
-$list->insert(13, $after);
-$list->delete($list->search(9));
-$list->print();
+//$after = $list->search(7);
+//$list->insert(13, $after);
+//$list->delete($list->search(9));
+//$list->print();
